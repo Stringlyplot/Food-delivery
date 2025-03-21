3d39cc9bb849832ca575dad31c9ff93e30a53b1d
Michael Menjivar Alvarado <michaelmenjivar456@gmail.com>
2025-03-21 13:05:40 -0500

    Added new text, print statements, and more

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
-def intro():
-    show = print("what is up gang")
-    return show
-    
-show = intro()
-intro()
+
+
+resturant_menu_items = [{"1": 7.95, "2": 9.30, "3": 8.50}, {"1": 13.25, "2": 16.50, "3": 12.99}]
+menu_choice = 1
+
+print (resturant_menu_items[menu_choice])
--- stringlydash.py
+++ stringlydash.py
@@ -1,26 +1,19 @@
 
-resturants = ["Chipotle", "Q39"]
+resturants = ["Welcome to Chipotle", "Welcome to Q39", "Chiptole & Q39"]
 #list in a dictinoary for the prices of each item
 resturant_menu_items = [{"1": 7.95, "2": 9.30, "3": 8.50}, {"1": 13.25, "2": 16.50, "3": 12.99}]
-
-def Chipotle_menu():
+def chipotle_menu():
     #chipotle menu
-    print("\nChipotle menu: \n1 - Chicken burrito $7.95 \n2 - Steak burrito bowl $ 9.30 \n3- Chicken Quesadilla $8.50\n\nEnter 1/2/3 to add items to your order")
-    input(">>> ")
-
-def Q39_menu():
+    chipotle = ("\nChipotle menu: \n1 - Chicken burrito $7.95 \n2 - Steak burrito bowl $ 9.30 \n3- Chicken Quesadilla $8.50\n\nEnter 1/2/3 to add items to your order")
+    return chipotle
+def q39_menu():
     #Q39 menu
-    while True:
-        print("\nQ39 menu: \n1 - Wings $13.25\n2 - Burnt End Burger $ 16.50\n3 - Brisket $12.99\n\nEnter 1/2/3 to add items to your order")
-        choice = int(input(">>>"))
-        final_decision = input("Anything else (Y/N): ")
-        order = resturant_menu_items[choice - 1][choice - 1]
-        if final_decision.lower in ["yes", "y", "ye", "ys", "es", "yess", "yesss"]:
-            pass
-        elif final_decision.lower in ["no", "n", "o", "noo", "nooo"]:
-            break
+    q39 = ("\nQ39 menu: \n1 - Wings $13.25\n2 - Burnt End Burger $ 16.50\n3 - Brisket $12.99\n\nEnter 1/2/3 to add items to your order")
+    return q39
+#Will pick the correct resturant
 
-    return
+menus = {1: chipotle_menu, 2: q39_menu}
+
 
 #Begining
 def introduction():
@@ -28,30 +21,27 @@ def introduction():
     print ("\nWelcome to 101 delivery")
     print("We devliver from the following 2 resturants\n")
     print("1 - Chipotle, 2 - Q39\n\nEnter your choice:\n1. Chipotle\n2. Q39\n3. Both")
-    #
+    #Where the user will pick the resturant
     user_resturant_choice = int(input(">>> "))
+    #This will return the users choice of resturant either chipotle or Q39 or both
     return user_resturant_choice
+#This is where the order is going to mainly be and will call the other functions
+def overall_order(menus_choice):
+     #This checks the menu list for th functions and calls it with the parenthesis at the end
+    print (menus[menus_choice]())
+    #Asks user for there choice of menu
+    order = input(">>> ")
+    #picks the correct menu and menu order
+    resturant_menu_items[menus_choice][order]
 
-def overall_order():
+    
+
+
     
     
     pass
 
 user_resturant_choice = introduction()
-overall_order()
+overall_order(user_resturant_choice)
 #Picks the resturant that the user picks
-print(resturants[user_resturant_choice - 1])
 
-
-
-
-
-
-
-
-"""
-1- Chicken burrito $7.95
-2- Steak burrito bowl $ 9.30
-3- Chicken Quesadilla $8.50
-Enter 1/2/3 to add items to your order
-"""
